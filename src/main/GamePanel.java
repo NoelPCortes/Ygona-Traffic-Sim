483731ba5f334b8ac482560e93a04269ec6b3875
(Arc)Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2026-04-24 19:32:49 +0800

    chore: added game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package src.main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     //SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
@@ -15,11 +15,24 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 384 pixels
     final int screenHeight = tileSize * maxScreenRow; //768 pixels
 
+    Thread gameThread;
+
+    //SETS SCREEN
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start(); //Calls run method (in-case I forgot)
+    }
+
+    @Override
+    public void run(){
 
     }
 
