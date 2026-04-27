92eab1fe0b91f3afc09e33f3c5649ae3f31d04f3
Chene2 <buenchen2@gmail.com>
2026-04-27 17:27:23 +0800

    feat: implement basic rendering and player movement logic

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,7 +15,16 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 384 pixels
     final int screenHeight = tileSize * maxScreenRow; //768 pixels
 
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     //SETS SCREEN
     public GamePanel() {
@@ -23,6 +32,8 @@ public class GamePanel extends JPanel implements Runnable {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
 
@@ -33,7 +44,57 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run(){
+        double drawInterval = 1000000000/FPS; // draw the screen for 0.0166666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
 
+        while(gameThread != null) {
+
+            // update information such as character positions
+            update();
+            // draw the screen with the updated information
+            repaint();
+        
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime(); // time remaining until nextDrawTime
+                remainingTime /= 1000000;
+
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long)remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+        
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update() {
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        } 
+
+        // stops from moving off the top of screen
+        if(playerY < 0) {
+            playerY = 0;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        
+        super.paintComponent(g);
+
+        Graphics g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 
 }
---
+++ src/main/KeyHandler.java
@@ -0,0 +1,28 @@
+package src.main;
+
+import java.awt.RenderingHints.Key;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener{
+
+    public boolean upPressed;
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+    }
+}
