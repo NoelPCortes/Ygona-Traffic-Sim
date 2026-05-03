280712eef9381010f36dd7b1b9719270c7e308a5
(Arc)Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2026-05-03 10:42:25 +0800

    chore: added player image(Incomplete)

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,8 +1,13 @@
 package src.entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
     public int x, y;
     public int speed;
+
+    public BufferedImage car;
+    public String direction;
 
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package src.entity;
 import src.main.GamePanel;
 import src.main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
 
@@ -27,6 +30,18 @@ public class Player extends Entity {
 
     }
 
+    public void getPlayerImage() {
+
+        try {
+
+            car = ImageIO.read(getClass().getResourceAsStream("/res/playerSprite/car3.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public void update() {
 
         if(keyH.upPressed == true) {
@@ -37,9 +52,9 @@ public class Player extends Entity {
 
     public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.WHITE);
+        BufferedImage image = car;
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        g2.drawImage(image, x, y, gp.tileSize * 2, gp.tileSize * 2, null);
 
     }
 
